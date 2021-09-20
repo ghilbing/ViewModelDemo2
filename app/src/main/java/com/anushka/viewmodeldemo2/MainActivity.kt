fa26f3e3ba9058a35f4e9c0900cf80c7dbb72d8f
Gretel Hilbing <ghilbing@gmail.com>
2021-09-20 13:23:17 -0700

    LiveData and MutableLiveData

--- app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
+++ app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.anushka.viewmodeldemo2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.anushka.viewmodeldemo2.databinding.ActivityMainBinding
 
@@ -17,11 +18,14 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
 
+        viewModel.totalData.observe(this, Observer {
+            binding.resultTV.text = it.toString()
+        })
+
         binding.apply {
-            resultTV.text = viewModel.getTotal().toString()
             addBT.setOnClickListener{
                 viewModel.setTotal(numberET.text.toString().toInt())
-                resultTV.text = viewModel.getTotal().toString()
+
             }
         }
 
--- app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
+++ app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
@@ -1,18 +1,20 @@
 package com.anushka.viewmodeldemo2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int): ViewModel() {
-    private var total = 0
+    private var total = MutableLiveData<Int>()
+    //this is to allow MutableLiveData be accessed from MainActivity
+    val totalData : LiveData<Int>
+        get() = total
 
     init {
-        total = startingTotal
+        total.value = startingTotal
     }
 
-    fun getTotal(): Int {
-        return total
-    }
     fun setTotal(input: Int) {
-        total += input
+        total.value = (total.value)?.plus(input)
     }
 }
